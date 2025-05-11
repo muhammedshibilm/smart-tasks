f7f52c2d920d8908cfc11cb5a8f7444156596a5d
Muhammed Shibil M <95218558+muhammedshibilm@users.noreply.github.com>
2025-05-11 15:26:21 +0530

    feat: initialize smarter-tasks project with React, TypeScript, and Tailwind CSS
    - Added package.json with dependencies and scripts for development and build
    - Configured PostCSS with Tailwind CSS
    - Created initial SVG logo for the project
    - Implemented basic styling in App.css
    - Developed main App component to render TaskApp
    - Created Task component to display individual tasks
    - Built TaskApp component to manage task state and rendering
    - Designed TaskForm component for adding new tasks
    - Implemented TaskList component to display a list of tasks
    - Added basic styling for Task components in TaskCard.css
    - Configured Tailwind CSS in index.css
    - Set up TypeScript types for task items
    - Configured Vite for development and build processes
    - Established TypeScript configuration files for app and node

---
+++ src/TaskForm.tsx
@@ -0,0 +1,95 @@
+import React, { type FormEventHandler } from "react";
+import type { TaskItems } from "./types";
+
+interface TaskFormProps {
+  addTask: (task: TaskItems) => void;
+}
+interface TaskFormSate {
+  title: string;
+  description: string;
+  dueDate: string;
+}
+class TaskForm extends React.Component<TaskFormProps, TaskFormSate> {
+  constructor(props: TaskFormProps) {
+    super(props);
+    this.state = {
+      title: "",
+      description: "",
+      dueDate: "",
+    };
+  }
+  addTask: FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+
+    const newTask = {
+      title: this.state.title,
+      description: this.state.description,
+      dueDate: this.state.dueDate,
+    };
+    this.props.addTask(newTask);
+    this.setState({ title: "", description: "", dueDate: "" });
+  };
+
+  titlechanged: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    this.setState({
+      title: event.target.value,
+    });
+  };
+
+  descriptionchanged: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    this.setState({
+      description: event.target.value,
+    });
+  };
+
+  duedatechanged: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    this.setState({
+      dueDate: event.target.value,
+    });
+  };
+  render(): React.ReactNode {
+    return (
+      <form
+        onSubmit={this.addTask}
+        className="space-y-4 flex flex-col justify-center"
+      >
+        <input
+          className="border-2 border-gray-500 rounded-sm  p-1"
+          placeholder="Enter the titile"
+          type="text"
+          id="todoTitle"
+          value={this.state.title}
+          onChange={this.titlechanged}
+          required
+        />
+        <input
+          type="text"
+          className="border-2 border-gray-500 rounded-sm p-1"
+          placeholder="Enter the description"
+          id="todoDescription"
+          onChange={this.descriptionchanged}
+          value={this.state.description}
+          required
+        />
+        <input
+          type="text"
+          className="border-2 border-gray-500 rounded-sm p-1"
+          placeholder="Enter the duedate "
+          id="todoDueDate"
+          value={this.state.dueDate}
+          onChange={this.duedatechanged}
+          required
+        />
+        <button
+          type="submit"
+          id="addTaskButton"
+          className="border  rounded-md  bg-gray-500 text-white px-4 py-2 block"
+        >
+          Add item
+        </button>
+      </form>
+    );
+  }
+}
+
+export default TaskForm;
